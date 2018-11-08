f4861d09bfb0371e43f8b72edfcbc51467a5765d
quidelantoine <quidelantoine@gmail.com>
2018-11-08 14:40:47 +0100

    genressss

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
 <?php include ('inc/pdo.php') ?>
 <?php include ('inc/fonction.php') ?>
 
-<?php $title = 'Accueil';
+<?php
+$sql = "SELECT genres FROM movies_full";
+$query = $pdo ->prepare ($sql);
+$query -> execute ();
+$genres = $query->fetchAll();
+
+//print_r($genres);
+$tableau = array();
+foreach ($genres as $genre) {
+  $g =  $genre['genres'];
+  $explode = explode(',',$g);
+  foreach ($explode as $ex) {
+    $ex = trim($ex);
+    if(!in_array($ex,$tableau)) {
+      if(!empty($ex)) {
+        $tableau[] = $ex;
+      }
+    }
+  }
+}
+print_r($tableau);
+
+
+ $title = 'Accueil';
 $sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 4";
 $query = $pdo ->prepare ($sql);
 $query -> execute ();
 $movies = $query->fetchAll();
+
+
 
 
 
