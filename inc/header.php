6cb6fead6419b2f4c3a8c8ea487f2e5882ac14bf
FermeyQ <fermeyquentin@gmail.com>
2018-11-08 10:56:10 +0100

    styleenmoins

--- inc/header.php
+++ inc/header.php
@@ -4,12 +4,11 @@
     <meta charset="utf-8">
     <title><?php echo $title ?></title>
     <link rel="stylesheet" href="asset/css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   </head>
   <body>
     <nav>
       <ul>
       <li><a href="index.php">Accueil</a></li>
-      <li><a href="detail.php">Detail</a></li>
+      <li><a href="detail.php">Détail</a></li>
       </ul>
     </nav>
