87aa203c101a1785dd54b552ed5a9c63922e369e
anthonydasil <anthony.gp2013@gmail.com>
2018-11-09 15:51:01 +0100

    7

--- inc/header.php
+++ inc/header.php
@@ -6,17 +6,19 @@
     <link rel="stylesheet" href="asset/css/style.css">
   </head>
   <body>
-    <nav>
-      <ul>
-      <li><a href="index.php">Accueil</a></li>
-      <li><a href="admin/dashboard.php">Dashboard</a></li>
-      <?php if (isLogged()){ ?>
-        <li><a href="filmsavoir.php">Films a voir !</a></li>
-        <li><a href="deconnection.php">Déconnexion</a></li>
-        <li>Bonjour <?php echo $_SESSION['user']['pseudo']; ?></li>
-      <?php }else{ ?>
-        <li><a href="inscription.php">Inscription</a></li>
-        <li><a href="connection.php">Connexion</a></li>
-      <?php } ?>
-      </ul>
-    </nav>
+<header>
+  <nav>
+    <ul>
+    <li><a href="index.php">Accueil</a></li>
+    <li><a href="admin/dashboard.php">Dashboard</a></li>
+    <?php if (isLogged()){ ?>
+      <li><a href="filmsavoir.php">Films a voir !</a></li>
+      <li><a href="deconnection.php">Déconnexion</a></li>
+      <li>Bonjour <?php echo $_SESSION['user']['pseudo']; ?></li>
+    <?php }else{ ?>
+      <li><a href="inscription.php">Inscription</a></li>
+      <li><a href="connection.php">Connexion</a></li>
+    <?php } ?>
+    </ul>
+  </nav>
+</header>
