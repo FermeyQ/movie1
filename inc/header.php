ce50a17ddb6d0a5d2cb10fdd19f58c606c693e07
unknown <hobyfln>
2018-11-09 12:03:29 +0100

    push gui

--- inc/header.php
+++ inc/header.php
@@ -6,22 +6,11 @@
     <link rel="stylesheet" href="asset/css/style.css">
   </head>
   <body>
-<<<<<<< HEAD
-    <header>
-      <nav>
-        <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="inscription.php">Inscription</a></li>
-        <li><a href="connection.php">Connexion</a></li>
-        </ul>
-      </nav>
-    </header>
-=======
     <nav>
       <ul>
       <li><a href="index.php">Accueil</a></li>
       <?php if (isLogged()){ ?>
-      <li><a href="filmsavoir.php">Films a voir !</a></li>
+        <li><a href="filmsavoir.php">Films a voir !</a></li>
         <li><a href="deconnection.php">Déconnexion</a></li>
         <li>Bonjour <?php echo $_SESSION['user']['pseudo']; ?></li>
       <?php }else{ ?>
@@ -30,4 +19,3 @@
       <?php } ?>
       </ul>
     </nav>
->>>>>>> 13a126bea61a153470cbbe951935494ab6a9cf9f
